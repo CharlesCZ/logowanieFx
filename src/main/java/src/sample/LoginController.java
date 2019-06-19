5a177ba05eb867502eff6c90f42339eb2f64b684
CharlesCZ <karakankaczyn@gmail.com>
2019-06-20 00:29:37 +0200

    login

--- src/main/java/src/sample/LoginController.java
+++ src/main/java/src/sample/LoginController.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import src.model.DatabaseVoip;
 import src.model.User;
@@ -13,6 +14,7 @@ import src.model.User;
 import java.io.IOException;
 import java.sql.Date;
 import java.time.LocalDate;
+import java.util.List;
 
 
 public class LoginController {
@@ -33,13 +35,31 @@ public class LoginController {
     private TextField registerConfPasswordFieldId;
 
     @FXML
+    private Text textUsername;
+
+    @FXML
+    private  Text passwordMisMatch;
+
+    @FXML
+    private Text passwordMisMatch2;
+
+    @FXML
+    private Text statusRegisterId;
+
+    @FXML
+    private Text loginText;
+
+    @FXML
     private void onLoginAction() throws IOException {
-
+        loginText.setText("");
         DatabaseVoip d=new DatabaseVoip();
-        d.insertUser(loginUserFieldId.getText(),loginPasswordFieldId.getText());
-        User user=d.selectUser(loginUserFieldId.getText(),loginPasswordFieldId.getText());
 
-        d.insertHistoryConnection(user.getId(),"sip sendera","sip invitera",new Date(System.currentTimeMillis()),new Date(System.currentTimeMillis()));
+        User user=d.selectUser(loginUserFieldId.getText(),GFG.encryptThisString(loginPasswordFieldId.getText()));
+if(user==null){
+    loginText.setText("Wrong login or password");
+    return;
+}
+       // d.insertHistoryConnection(user.getId(),"sip sendera","sip invitera",new Date(System.currentTimeMillis()),new Date(System.currentTimeMillis()));
 
         System.out.println(loginUserFieldId.getText());
 
@@ -55,8 +75,27 @@ public class LoginController {
 
     @FXML
     private void onRegisterAction(){
+        textUsername.setText("");
+        passwordMisMatch.setText("");
+        passwordMisMatch2.setText("");
+        statusRegisterId.setText("");
 
+        DatabaseVoip d=new DatabaseVoip();
+        List<User> users=d.selectUsers();
+       if( users.stream().anyMatch(user -> user.getLogin().equals(registerUserFieldId.getText()))) {
+           textUsername.setText("Login already exists");
+            return;
+       }
 
+        if( !registerPasswordFieldId.getText().equals(registerConfPasswordFieldId.getText())) {
+            passwordMisMatch.setText("Password mismatch");
+            passwordMisMatch2.setText("Password mismatch");
+            return;
+        }
+
+        d.insertUser(registerUserFieldId.getText(),GFG.encryptThisString(registerPasswordFieldId.getText()));
+        d.closeConnection();
+        statusRegisterId.setText("Account created!");
 
     }
 }
